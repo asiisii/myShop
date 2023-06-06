3763f9535b73a908d54f6e79e9931f2e6f28ea3b
asiisii <ashishmalla45@gmail.com>
2023-06-06 17:39:35 -0600

    update home and product page

---
+++ frontend/src/product1.js
@@ -0,0 +1,122 @@
+const products = [
+    {
+      _id: '1',
+      name: 'Samsung 55" 4K Ultra HD Smart TV',
+      image: '/images/samsung-tv.jpg',
+      description:
+        'Experience stunning picture quality with this 55" 4K Ultra HD Smart TV from Samsung. Enjoy your favorite shows and movies with incredible clarity and vibrant colors.',
+      brand: 'Samsung',
+      category: 'Electronics',
+      price: 799.99,
+      countInStock: 8,
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: '2',
+      name: 'Bose QuietComfort 35 Wireless Headphones',
+      image: '/images/bose-headphones.jpg',
+      description:
+        'Immerse yourself in your favorite music with the Bose QuietComfort 35 Wireless Headphones. The noise-canceling technology ensures a distraction-free and immersive listening experience.',
+      brand: 'Bose',
+      category: 'Electronics',
+      price: 299.99,
+      countInStock: 5,
+      rating: 4.0,
+      numReviews: 6,
+    },
+    {
+      _id: '3',
+      name: 'DJI Mavic Air 2 Drone',
+      image: '/images/dji-drone.jpg',
+      description:
+        'Capture stunning aerial shots with the DJI Mavic Air 2 Drone. It offers 4K video recording, advanced obstacle avoidance, and a range of intelligent flight modes.',
+      brand: 'DJI',
+      category: 'Electronics',
+      price: 999.99,
+      countInStock: 3,
+      rating: 4.8,
+      numReviews: 15,
+    },
+    {
+      _id: '4',
+      name: 'LG 27" UltraGear Gaming Monitor',
+      image: '/images/lg-monitor.jpg',
+      description:
+        'Upgrade your gaming setup with the LG 27" UltraGear Gaming Monitor. It features a high refresh rate, quick response time, and stunning visuals to enhance your gaming experience.',
+      brand: 'LG',
+      category: 'Electronics',
+      price: 499.99,
+      countInStock: 6,
+      rating: 4.6,
+      numReviews: 9,
+    },
+    {
+      _id: '5',
+      name: 'Nikon D850 DSLR Camera',
+      image: '/images/nikon-camera.jpg',
+      description:
+        'Capture exceptional photos and videos with the Nikon D850 DSLR Camera. It offers a high-resolution sensor, fast autofocus, and advanced shooting capabilities for professional-level results.',
+      brand: 'Nikon',
+      category: 'Electronics',
+      price: 2799.99,
+      countInStock: 2,
+      rating: 4.9,
+      numReviews: 20,
+    },
+    {
+      _id: '6',
+      name: 'Apple Watch Series 6',
+      image: '/images/apple-watch.jpg',
+      description:
+        'Stay connected and track your fitness goals with the Apple Watch Series 6. It features a vibrant display, advanced health monitoring, and a wide range of smart features.',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 399.99,
+      countInStock: 4,
+      rating: 4.3,
+      numReviews: 7,
+    },
+    {
+      _id: '7',
+      name: 'Sony WH-1000XM4 Wireless Noise-Canceling Headphones',
+      image: '/images/sony-headphones.jpg',
+      description:
+        'Enjoy superior sound quality and immersive listening experience with the Sony WH-1000XM4 Wireless Noise-Canceling Headphones. The advanced noise-canceling technology blocks out ambient noise for an undisturbed audio experience.',
+      brand: 'Sony',
+      category: 'Electronics',
+      price: 349.99,
+      countInStock: 8,
+      rating: 4.7,
+      numReviews: 13,
+    },
+    {
+      _id: '8',
+      name: 'HP Pavilion 15.6" Gaming Laptop',
+      image: '/images/hp-laptop.jpg',
+      description:
+        'Experience powerful gaming performance with the HP Pavilion 15.6" Gaming Laptop. It features a high-performance processor, dedicated graphics, and a vibrant display for an immersive gaming experience.',
+      brand: 'HP',
+      category: 'Electronics',
+      price: 999.99,
+      countInStock: 4,
+      rating: 4.4,
+      numReviews: 11,
+    },
+    {
+      _id: '9',
+      name: 'Beats Solo3 Wireless On-Ear Headphones',
+      image: '/images/beats-headphones.jpg',
+      description:
+        'Enjoy your favorite music with the Beats Solo3 Wireless On-Ear Headphones. The sleek design, long battery life, and exceptional sound quality make it perfect for on-the-go listening.',
+      brand: 'Beats',
+      category: 'Electronics',
+      price: 199.99,
+      countInStock: 6,
+      rating: 4.2,
+      numReviews: 8,
+    },
+  ];
+  
+  export default products;
+  
